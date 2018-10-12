9fae34f979a2613b005bba7ff85badba8e20ec6c
lauren sambu <laurensambu@gmail.com>
2018-10-12 18:49:54 +0300

    made changes and connected to db

--- app.py
+++ app.py
@@ -1,36 +1,44 @@
 #!flask/Source/python 
 import mysql.connector
-from  flask import Flask
-from mysql.connector import errorcode 
+from  flask import Flask, jsonify 
+from mysql.connector import errorcode
 
-try:
-  connection= mysql.connector.connect(
-	host= 'localhost',
-	user='root',
-	password= '',
-	database='todo_list',
 
-	)
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  connection.close()
+def mysql_conn():
+  try:
+    connection= mysql.connector.connect(
+  	 host= 'localhost',
+  	 user='root',
+  	 password= 'babydaddy1',
+  	 database='todo_list',
+  	 )
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+       print("Database does not exist")
+    else:
+       print(err)
+    
+  return connection
+
 app = Flask ('app')
-@app.route('/') 
-
-
-
+@app.route('/')
 def index():
-	return 'Miss me?'
+	return 'Miss me ?'
+
+@app.route('/user',methods=['GET']) 
+def get_users(): 
+  conn = mysql_conn()
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM user")
+  result = cursor.fetchall()
+
+  return jsonify (result) 
 
 
  
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
 	app.run(debug=True)
 
